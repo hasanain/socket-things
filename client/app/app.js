4537b39a75cbaa8fb752857d6f6a03bb439f3b76
Hasanain Jamal <hasanain.jm@gmail.com>
2016-09-02 22:01:41 -0700

    Added functionality for a chat client

--- client/app/app.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('socketThings', ['ngRoutes']);
-
-app.config(function ($routeProvider, $locationProvider) {
-
-});
--- server/app.js
+++ server/app.js
@@ -1,21 +1,16 @@
 /*jslint unparam: true, node: true */
 var express = require('express');
 var app = express();
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
 var path = require('path');
-var fs = require('fs');
 var server = require('http').Server(app);
+
+var port = process.env.PORT || 8080;
 
 var io = require('socket.io')(server);
 var router = express.Router();
-var apiRouter = express.Router();
-var port = process.env.PORT || 8080;
 
 var list = ["item 1", "item 2"];
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
 // code to get around CORS
 app.use(function (req, res, next) {
@@ -25,19 +20,16 @@ app.use(function (req, res, next) {
   next();
 });
 
-// routes for the api
-apiRouter.get('/', function (req, res) {
-  res.json({
-    message: 'I am the API!'
-  });
-});
 
 // routes for the static pages
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/../client/index.html'));
 });
-app.get('/test', function(req, res){
-  res.sendFile(path.join(__dirname + '/../client/test.html'));
+app.get('/todo', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../client/todo.html'));
+});
+app.get('/chat', function(req, res){
+  res.sendFile(path.join(__dirname + '/../client/chat.html'));
 });
 app.get('/files/listSocketClient.js', function (req, res) {
   res.sendFile(path.join(__dirname + '/../client/listSocketClient.js'));
@@ -46,13 +38,14 @@ app.get('/files/listSocketClient.js', function (req, res) {
 
 // set up routers with server
 app.use(express.static(__dirname + '../client'));
-app.use('/api', apiRouter);
 
 server.listen(port);
 
 console.log('Server is running on port %d', port);
 
-io.on('connection', function (socket) {
+// TODO list socket
+io.of('/todo')
+  .on('connection', function (socket) {
   socket.emit('user-connected', list);
 
   socket.on('new-item-added', function (data) {
@@ -60,7 +53,23 @@ io.on('connection', function (socket) {
     socket.emit('new-item-added-client', data);
     socket.broadcast.emit('new-item-added-client', data);
   });
-  socket.on('typing-client', function (data) {
-    socket.broadcast.emit('typing-client');
+});
+
+// chat app socket
+var messages = [];
+var connected = [];
+io.of('/chat')
+  .on('connection', function(socket){
+  var name = socket.request._query['name'];
+  if(name !== "") connected.push(name);
+  socket.emit('user-connected', {messages: messages, connected: connected, user: name});
+  socket.broadcast.emit('new-user-joined', {user:name});
+  socket.on('new-message',function(data){
+    messages.push(data);
+    socket.broadcast.emit('new-message', data);    
+  });
+  socket.on('disconnect', function(){
+    connected.splice(connected.indexOf(name), 1);
+    socket.broadcast.emit('user-left', name);
   });
 });
